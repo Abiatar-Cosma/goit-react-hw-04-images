622f51befebb0e72dd794857ef255c7da3a5a13f
Abiatar Cosma <abiatar.cosma@yahoo.com>
2025-02-02 14:42:03 +0100

    react hooks update

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,12 @@
+import { memo } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-const Button = ({ onClick }) => {
-  return (
-    <button className={styles.Button} onClick={onClick}>
-      Load more
-    </button>
-  );
-};
+const Button = memo(({ onClick }) => (
+  <button className={styles.Button} onClick={onClick}>
+    Load more
+  </button>
+));
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,17 +1,15 @@
 import PropTypes from 'prop-types';
 import styles from './ImageGalleryItem.module.css';
 
-const ImageGalleryItem = ({ smallImage, largeImage, onClick }) => {
-  return (
-    <li className={styles.ImageGalleryItem} onClick={() => onClick(largeImage)}>
-      <img
-        className={styles.ImageGalleryItemImage}
-        src={smallImage}
-        alt="Gallery item"
-      />
-    </li>
-  );
-};
+const ImageGalleryItem = ({ smallImage, largeImage, onClick }) => (
+  <li className={styles.ImageGalleryItem} onClick={() => onClick(largeImage)}>
+    <img
+      className={styles.ImageGalleryItemImage}
+      src={smallImage}
+      alt="Gallery item"
+    />
+  </li>
+);
 
 ImageGalleryItem.propTypes = {
   smallImage: PropTypes.string.isRequired,
@@ -20,4 +18,3 @@ ImageGalleryItem.propTypes = {
 };
 
 export default ImageGalleryItem;
-
